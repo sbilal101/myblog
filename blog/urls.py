dda17a818c65b624e3721f0da852227f10f2729f
Bilal <sbilal101@gmail.com>
2024-11-16 23:34:50 +0300

    Change the HTML for site.

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    #path("", views.home, name="home"),
+    path('', views.post_list, name='post_list')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def home(request):
-    return HttpResponse("Hello Django!")
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
