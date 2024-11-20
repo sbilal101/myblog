ccef9b1a2c31dc3a665965eb73158e36745d2682
Bilal <sbilal101@gmail.com>
2024-11-21 00:35:15 +0300

    add forms and ,post_edit

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     #path("", views.home, name="home"),
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
 
 ]
